8d48ea30b01f89a8a01937bb5ba43a1330370fb7
fhluo <20843637+fhluo@users.noreply.github.com>
2023-03-03 00:40:24 +0800

    feat: generate pkg/wishes/types.go

--- pkg/wish/type.go
+++ pkg/wish/type.go
@@ -1,6 +1,16 @@
 package wish
 
+import (
+	"golang.org/x/text/cases"
+	"golang.org/x/text/language"
+	"regexp"
+)
+
 type Type struct {
 	Key  int    `json:"key,string" mapstructure:"key"`
 	Name string `json:"name" mapstructure:"name"`
+}
+
+func (t Type) VarName() string {
+	return regexp.MustCompile(`-+|\s+`).ReplaceAllString(cases.Title(language.English).String(t.Name), "")
 }
