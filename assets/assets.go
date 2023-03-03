1fbec90939a5dbd5015916ff1963198c1adb6b4d
fhluo <20843637+fhluo@users.noreply.github.com>
2023-03-04 01:37:04 +0800

    refactor: use sync.Once

--- assets/assets.go
+++ assets/assets.go
@@ -2,14 +2,15 @@ package assets
 
 import (
 	_ "embed"
-)
-
-var (
-	//go:embed assets.json
-	AssetsJSON string
+	"github.com/bytedance/sonic"
+	"github.com/samber/lo"
+	"sync"
 )
 
 //go:generate go run ../cmd/giwh-dev dl assets
+
+//go:embed assets.json
+var JSON []byte
 
 type Assets struct {
 	Characters map[string]string `json:"characters"`
@@ -21,4 +22,16 @@ func New() Assets {
 		Characters: make(map[string]string),
 		Weapons:    make(map[string]string),
 	}
+}
+
+var (
+	once   sync.Once
+	assets = New()
+)
+
+func Get() Assets {
+	once.Do(func() {
+		lo.Must0(sonic.Unmarshal(JSON, &assets))
+	})
+	return assets
 }
