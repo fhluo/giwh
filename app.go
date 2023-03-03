f61900987132a9ece4bcc4dd9b2744e3dc2d5bf2
fhluo <20843637+fhluo@users.noreply.github.com>
2023-03-04 01:43:25 +0800

    refactor: pass JSON strings directly

--- app.go
+++ app.go
@@ -1,47 +1,58 @@
 package main
 
 import (
+	assetsPkg "github.com/fhluo/giwh/assets"
+	"github.com/fhluo/giwh/i18n"
 	"github.com/fhluo/giwh/internal/config"
-	"github.com/fhluo/giwh/pkg/api"
-	"github.com/fhluo/giwh/pkg/i18n"
-	"github.com/fhluo/giwh/pkg/repository"
-	"github.com/fhluo/giwh/pkg/repository/primitive"
+	"github.com/fhluo/giwh/pkg/wish"
 	"golang.org/x/net/context"
-	"log"
+	"os"
+	"strconv"
+	"unsafe"
 )
 
 type App struct {
 	ctx context.Context
-	repository.Repository
 }
 
 func NewApp() *App {
-	r, err := primitive.Load(config.WishHistoryPath)
-	if err != nil {
-		log.Fatalln(err)
-	}
-	return &App{Repository: r}
+	return &App{}
 }
 
 func (a *App) startup(ctx context.Context) {
 	a.ctx = ctx
+}
+
+func (a *App) GetDefaultSharedWish() string {
+	return strconv.Itoa(wish.CharacterEventWishAndCharacterEventWish2.Key)
+}
+
+func (a *App) GetAssets() string {
+	return unsafe.String(unsafe.SliceData(assetsPkg.JSON), len(assetsPkg.JSON))
+}
+
+func (a *App) GetLanguage() string {
 	if config.Language.Get() != "" {
-		i18n.Language = config.Language.Get()
+		return i18n.Match(config.Language.Get()).Key
+	} else {
+		return i18n.Default().Key
 	}
 }
 
-func (a *App) GetWishName(wishType api.WishType) string {
-	return i18n.GetWishName(wishType.Str())
+func (a *App) GetLocale(lang string) string {
+	data, err := i18n.ReadLocaleFile(i18n.Match(lang))
+	if err != nil {
+		return ""
+	}
+
+	return unsafe.String(unsafe.SliceData(data), len(data))
 }
 
-func (a *App) GetSharedWishName(wishType api.SharedWishType) string {
-	return i18n.GetSharedWishName(wishType.Str())
-}
+func (a *App) GetItems() string {
+	data, err := os.ReadFile(config.WishHistoryPath)
+	if err != nil {
+		return ""
+	}
 
-func (a *App) GetPity(rarity api.Rarity, wishType api.SharedWishType) int {
-	return wishType.Pity(rarity)
-}
-
-func (a *App) GetSharedWishTypes() []api.SharedWishType {
-	return api.SharedWishTypes
+	return unsafe.String(unsafe.SliceData(data), len(data))
 }
