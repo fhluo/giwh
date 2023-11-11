9cb585bbbca2c1a81be359369351edc4f5c4e89e
fhluo <20843637+fhluo@users.noreply.github.com>
2023-11-11 22:27:19 +0800

    refactor: use errors.As

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,12 +1,12 @@
 package config
 
 import (
+	"errors"
 	"github.com/fhluo/giwh/i18n"
 	"github.com/spf13/viper"
 	"log/slog"
 	"os"
 	"path/filepath"
-	"sync"
 )
 
 var (
@@ -15,62 +15,61 @@ var (
 	Path = filepath.Join(Dir, "config.toml")
 
 	WishHistoryPath = NewItem("wish_history_path", filepath.Join(Dir, "wish_history.json"))
+	DBPath          = NewItem("db_path", filepath.Join(Dir, "wish_history.json"))
 	Language        = NewItem("language", i18n.Default().Tag().String())
 )
 
 func init() {
-	_ = os.MkdirAll(Dir, 0666)
+	// 创建配置文件目录
+	err := os.MkdirAll(Dir, 0666)
+	if err != nil {
+		slog.Error(err.Error())
+		os.Exit(1)
+	}
 
+	// 添加配置文件目录，设置配置文件名
 	viper.AddConfigPath(Dir)
 	viper.SetConfigName("config")
 	viper.SetConfigType("toml")
 
-	if err := viper.ReadInConfig(); err != nil {
-		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
-			if err = viper.WriteConfigAs(Path); err != nil {
-				slog.Warn("failed to write config", "path", Path)
-			}
-		}
+	// 读取配置文件
+	err = viper.ReadInConfig()
+	if err == nil {
+		return
+	}
+
+	// 读取配置文件失败，判断错误类型
+	var notFoundErr viper.ConfigFileNotFoundError
+	if !errors.As(err, &notFoundErr) {
+		slog.Error(err.Error())
+		os.Exit(1)
+	}
+
+	// 未找到配置文件，创建配置文件
+	slog.Info(notFoundErr.Error())
+	if err = viper.WriteConfigAs(Path); err != nil {
+		slog.Error(err.Error())
+		os.Exit(1)
 	}
 }
 
-var mutex sync.Mutex
-
-type Item[T any] struct {
-	Key          string
-	DefaultValue T
-}
-
-func NewItem[T any](key string, defaultValue T) Item[T] {
-	viper.SetDefault(key, defaultValue)
-	return Item[T]{
-		Key:          key,
-		DefaultValue: defaultValue,
-	}
-}
-
-func (item Item[T]) Get() T {
-	mutex.Lock()
-	defer mutex.Unlock()
-
-	return viper.Get(item.Key).(T)
-}
-
-func (item Item[T]) Set(value T) {
-	mutex.Lock()
-	defer mutex.Unlock()
-
-	viper.Set(item.Key, value)
-}
-
+// Save 保存配置文件
 func Save() {
-	if err := viper.WriteConfig(); err != nil {
-		if _, ok := err.(viper.ConfigFileNotFoundError); !ok {
-			if err = viper.WriteConfigAs(Path); err != nil {
-				slog.Warn("failed to write config", "path", Path)
-			}
-		} else {
-			slog.Error(err.Error())
-		}
+	// 写入配置
+	err := viper.WriteConfig()
+	if err == nil {
+		return
+	}
+
+	// 写入配置失败，判断错误类型
+	var notFoundErr viper.ConfigFileNotFoundError
+	if !errors.As(err, &notFoundErr) {
+		slog.Warn(err.Error())
+		return
+	}
+
+	// 重新尝试写入配置
+	if err = viper.WriteConfigAs(Path); err != nil {
+		slog.Warn(err.Error())
 	}
 }
